efa2759393092b3f02721f1cdb4183e124dc471e
David Bowers <dnlbowers@gmail.com>
2024-01-05 19:35:08 +0100

    public and private methods

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,17 @@
-class Pizza2 {
-    name: string;
-    toppings: string[] = [];
-    constructor(name: string) {
-        this.name = name;
-    }
+class Pizza {
+    public toppings: string[] = [];
+    constructor(private name: string) {}
 
     addTopping(toppings: string) {
         this.toppings.push(toppings);
     }
 }
 
-function Pizza(name:string) {
-    this.name = name;
-    this.toppings = [];
-}
-
-Pizza.prototype.addTopping = function addTopping(toppings: string) {
-    this.toppings.push(toppings);
-};
 
 const pizza = new Pizza('Pepperoni');
 
 pizza.addTopping('pepperoni');
+pizza.addTopping('cheese');
 
 console.log(pizza);
 

a761d5c39093c6d09c6a9bdd9fa342a323c8701e
David Bowers <dnlbowers@gmail.com>
2024-01-05 18:48:39 +0100

    function compared with class

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,27 @@
-type Pizza = {
-    name: string,
-    toppings: number,
-};
+class Pizza2 {
+    name: string;
+    toppings: string[] = [];
+    constructor(name: string) {
+        this.name = name;
+    }
 
-const pizza: Pizza = {name: 'Blazing Inferno', toppings: 5};
-const serialized = JSON.stringify(pizza);
-function getNameFromJSON(obj: string) {
-    return (JSON.parse(obj) as Pizza).toppings;
+    addTopping(toppings: string) {
+        this.toppings.push(toppings);
+    }
 }
 
-const pizzaName = getNameFromJSON(serialized);
+function Pizza(name:string) {
+    this.name = name;
+    this.toppings = [];
+}
 
+Pizza.prototype.addTopping = function addTopping(toppings: string) {
+    this.toppings.push(toppings);
+};
+
+const pizza = new Pizza('Pepperoni');
+
+pizza.addTopping('pepperoni');
+
+console.log(pizza);
 

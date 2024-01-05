1d75b9857761561dc2ae96712c95d70c38e92b15
David Bowers <dnlbowers@gmail.com>
2024-01-05 23:23:31 +0100

    getters and setters

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,26 @@
+class Sizes {
+    constructor(public sizes: string[]) {}
+
+    set availableSizes(sizes: string[]) {
+        this.sizes = sizes;
+    }
+
+    get availableSizes() {
+        return this.sizes;
+    }
+}
+
+const sizes = new Sizes(['small', 'medium']);
+// invoke getter
+console.log(sizes.availableSizes)
+// invoke setter
+sizes.availableSizes = ['medium', 'large'];
+
+console.log(sizes.availableSizes)
+
 class Pizza {
     public toppings: string[] = [];
-    constructor(private name: string) {}
+    constructor(readonly name: string) {}
 
     addTopping(toppings: string) {
         this.toppings.push(toppings);
@@ -13,5 +33,5 @@ const pizza = new Pizza('Pepperoni');
 pizza.addTopping('pepperoni');
 pizza.addTopping('cheese');
 
-console.log(pizza);
+console.log(pizza.name);
 
